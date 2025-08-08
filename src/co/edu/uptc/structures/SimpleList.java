73a3ebcf555094089b86865af26f4d31e3b12b41
hoyospi95 <jorge.hoyos@uptc.edu.co>
2025-08-08 15:39:28 -0500

    codigo base

---
+++ src/co/edu/uptc/structures/SimpleList.java
@@ -0,0 +1,15 @@
+package co.edu.uptc.structures;
+
+public class SimpleList<T> {
+	private Node<T> head;
+	
+	public boolean add(T value) {
+		boolean added = false;
+		
+		return added;
+	}
+	
+	public boolean isEmpty() {
+		return head == null;
+	}
+}
